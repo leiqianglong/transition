a702d4662cfd73e531ed8d52d9578544af1edf5f
lql <582972330@qq.com>
2019-06-22 14:52:45 +0800

    添加学生类

---
+++ LQCustomerPresent/Stutent.m
@@ -0,0 +1,13 @@
+//
+//  Stutent.m
+//  LQCustomerPresent
+//
+//  Created by 龙强雷 on 2019/6/22.
+//  Copyright © 2019 龙强雷. All rights reserved.
+//
+
+#import "Stutent.h"
+
+@implementation Stutent
+
+@end
